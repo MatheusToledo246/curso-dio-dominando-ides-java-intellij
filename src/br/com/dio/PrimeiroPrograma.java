d905ed6ae71d55ff9f3e2ea30349a4ee18a0ebdf
MatheusToledo246 <matheus.silva246@fatec.sp.gov.br>
2023-03-13 10:42:17 -0300

    ceiando class livro

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -3,11 +3,50 @@ package br.com.dio;
 import br.com.dio.model.Gato;
 
 public class PrimeiroPrograma {
-    public static void main(String[] args) {
+    public void main(String[] args) {
         Gato gato = new Gato();
         System.out.println(gato);
+
+        livro livro1 = new livro("O problema dos 3 compos", 300);
+        System.out.println(livro1);
+
         /*int a = 5;
         int b = 3;
         System.out.println("Hellow Word! " + (a+b));*/
     }
+
+
+    class livro{
+        private String nome;
+        private Integer  numPaginas;
+
+        public livro(String nome, Integer numPaginas) {
+            this.nome = nome;
+            this.numPaginas = numPaginas;
+        }
+
+        public String getNome() {
+            return nome;
+        }
+
+        public void setNome(String nome) {
+            this.nome = nome;
+        }
+
+        public Integer getNumPaginas() {
+            return numPaginas;
+        }
+
+        public void setNumPaginas(Integer numPaginas) {
+            this.numPaginas = numPaginas;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "PrimeiroPrograma{}";
+    }
+
+
 }
+
